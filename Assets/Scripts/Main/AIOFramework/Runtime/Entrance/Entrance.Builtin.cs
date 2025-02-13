8341e77331f72bbf98423fd73a5b59ae6dca288f
junbai <jun_bai@droidhang.com>
2025-02-13 11:19:42 +0800

    Initial commit

---
+++ Assets/Scripts/Main/AIOFramework/Runtime/Entrance/Entrance.Builtin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace AIOFramework.Runtime
+{
+    public partial class Entrance : MonoBehaviour
+    {
+        public static BaseComponent Base { get; private set;}
+        public static ResourceComponent Resource { get; private set;}
+        private void InitBuiltinComponents()
+        {
+            Base = GameEntry.GetComponent<BaseComponent>();
+            Resource = GameEntry.GetComponent<ResourceComponent>();
+        }
+    }
+}
---
+++ Assets/Scripts/Main/AIOFramework/Runtime/Resource/ResourceComponent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using GameFramework;
+using UnityEngine;
+using YooAsset;
+
+namespace AIOFramework.Runtime
+{
+    public partial class ResourceComponent : GameFrameworkComponent
+    {
+        [SerializeField] 
+        private string m_PackageName = "DefaultPackage";
+        public string PackageName => m_PackageName; 
+        
+        private EPlayMode m_PlayMode = EPlayMode.EditorSimulateMode;
+        public EPlayMode PlayMode
+        {
+            get
+            {
+#if UNITY_EDITOR
+                return m_PlayMode;
+#elif UNITY_WEBGL
+                return EPlayMode.WebPlayMode;
+#else
+                return EPlayMode.HostPlayMode;
+#endif
+            }
+        }
+
+        [SerializeField]
+        [Tooltip("异步操作每帧最大时间切片(毫秒)")]
+        private long m_TimeSlice = 1000L;
+        public long TimeSlice => m_TimeSlice;
+        
+        protected override void Awake()
+        {
+            base.Awake();
+            Initialize();
+        }
+    }
+}
